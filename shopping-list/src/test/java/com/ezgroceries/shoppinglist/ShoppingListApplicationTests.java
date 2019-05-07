5d80f3f59af3b8b7dc7216139088cec9f98383f9
Matthieu Merlyn <matthieu.merlyn@kbc.be>
2019-05-07 17:38:43 +0200

    Spring MockMVC initial setup

--- shopping-list/src/test/java/com/ezgroceries/shoppinglist/ShoppingListApplicationTests.java
+++ shopping-list/src/test/java/com/ezgroceries/shoppinglist/ShoppingListApplicationTests.java
@@ -2,15 +2,29 @@ package com.ezgroceries.shoppinglist;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ShoppingListApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void getAllCocktails() throws Exception {
+        this.mockMvc.perform(get("/cocktails?search=Russian"));
     }
 
 }
