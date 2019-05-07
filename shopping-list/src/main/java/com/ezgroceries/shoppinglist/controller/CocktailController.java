a0ba22cc3226302d8dc723004e064f1f9cc47a32
Matthieu Merlyn <matthieu.merlyn@kbc.be>
2019-05-07 16:56:41 +0200

    Stub to add cocktails to shopping list

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/controller/CocktailController.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/controller/CocktailController.java
@@ -3,13 +3,11 @@ package com.ezgroceries.shoppinglist.controller;
 import com.ezgroceries.shoppinglist.model.Cocktail;
 import com.ezgroceries.shoppinglist.service.CocktailService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/cocktails", produces = "application/json")
@@ -22,6 +20,4 @@ public class CocktailController {
         return cocktailService.getAllCocktails();
     }
 
-    // @PostMapping
-    // public List<UUID> addCocktailsToList
 }
--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/controller/ShoppingListController.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/controller/ShoppingListController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,4 +38,9 @@ public class ShoppingListController {
         return shoppingListService.addShoppingList(newShoppingList);
     }
 
+    @PostMapping(value = "/{shoppingListId}/cocktails")
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<Cocktail> addCocktailsToShoppingList(@PathVariable String shoppingListId, @RequestBody List<Cocktail> cocktails) {
+        return cocktails;
+    }
 }
