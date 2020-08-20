5fb0061958a6355aae89127258aee6dc4d69889a
Matthieu Merlyn <matthieu.merlyn@kbc.be>
2020-08-20 17:08:42 +0200

    lab01

---
+++ shopping-list-new/src/main/java/com/ezgroceries/shoppinglistnew/ShoppingListNewApplication.java
@@ -0,0 +1,13 @@
+package com.ezgroceries.shoppinglistnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingListNewApplication.class, args);
+    }
+
+}
